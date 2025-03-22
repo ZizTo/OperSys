1266019f30d269d4b506f91ef1ccd7f62656ce9f
ZizTo <pashamatievski@gmail.com>
2025-03-22 17:50:39 +0300

    Lab3

---
+++ lab3/lab314/lab3.cpp
@@ -0,0 +1,85 @@
+﻿#include "lab3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "enter array size: ";
+	cin >> n;
+	mas = vector<int>(n, -1);
+
+	int thkol;
+	cout << "Eneter threads ammount: ";
+	cin >> thkol;
+	thrkol = thkol;
+
+	thread* hThreads = new thread[thkol];
+	StopEvent = new HANDLE[thkol];
+	ResumeEvent = new HANDLE[thkol];
+	DeleteEvent = new HANDLE[thkol];
+	isThreadWorks.resize(thkol, true);
+	
+	DeadThEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+	for (int i = 0; i < thkol; i++)
+	{
+		StopEvent[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+		ResumeEvent[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+		DeleteEvent[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+		hThreads[i] = thread(marker, i);
+	}
+
+	while (true)
+	{
+		WaitForMultipleObjects(thkol, StopEvent, TRUE, INFINITE);
+
+		cout << "mas before deleting: ";
+		for (int i = 0; i < n; i++)
+		{
+			cout << mas[i] << " ";
+		}
+
+		int thrId;
+		cout << endl << "Enter id of thread that need to stop: ";
+		cin >> thrId;
+		while (thrId >= n || thrId < 0 || !isThreadWorks[thrId]) {
+			cout << "This thread does not exist, enter antorher: ";
+			cin >> thrId;
+		}
+
+		ResetEvent(DeadThEvent);
+		SetEvent(DeleteEvent[thrId]);
+		WaitForSingleObject(DeadThEvent, INFINITE);
+
+		cout << "mas after deleting: ";
+		for (int i = 0; i < n; i++)
+		{
+			cout << mas[i] << " ";
+		}
+		cout << endl;
+
+		if (thrkol <= 0) {
+			cout << "end of cycle";
+			break;
+		}
+
+		for (int i = 0; i < thkol; i++)
+		{
+			if (isThreadWorks[i]) {
+				ResetEvent(StopEvent[i]);
+				SetEvent(ResumeEvent[i]);
+			}
+			else {
+				SetEvent(StopEvent[i]);
+			}
+		}
+	}
+
+	for (int i = 0; i < thkol; i++)
+	{
+		CloseHandle(StopEvent[i]);
+		CloseHandle(ResumeEvent[i]);
+		CloseHandle(DeleteEvent[i]);
+	}
+
+	return 0;
+}
---
+++ lab3/lab314/lab3.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+#include <windows.h>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <mutex>
+
+std::vector<int> mas;
+std::vector<bool> isThreadWorks;
+HANDLE* StopEvent;
+HANDLE* ResumeEvent;
+HANDLE* DeleteEvent;
+HANDLE DeadThEvent;
+std::mutex cs, csout;
+int n = 0;
+int thrkol = 0;
+
+DWORD WINAPI marker(int lpParam) {
+    srand(lpParam);
+    int elmarked = 0;
+
+    while (true) {
+        int index = rand() % n;
+
+        cs.lock();
+        if (mas[index] == -1) {
+            Sleep(5);
+            mas[index] = lpParam;
+            elmarked++;
+            cs.unlock();
+            Sleep(5);
+        }
+        else {
+            cs.unlock();
+
+            csout.lock();;
+            std::cout << "Thread id" << lpParam << " marked " << elmarked
+                << " elements. Stop on " << index << std::endl;
+            SetEvent(StopEvent[lpParam]);
+            csout.unlock();
+
+            while (true) {
+                if (WaitForSingleObject(DeleteEvent[lpParam], 0) != WAIT_TIMEOUT) {
+                    isThreadWorks[lpParam] = false;
+                    for (int i = 0; i < n; ++i) {
+                        if (mas[i] == lpParam) {
+                            mas[i] = -1;
+                        }
+                    }
+                    thrkol--;
+                    SetEvent(DeadThEvent);
+                    return 0;
+                }
+                else if (WaitForSingleObject(ResumeEvent[lpParam], 0) != WAIT_TIMEOUT) {
+                    ResetEvent(ResumeEvent[lpParam]);
+                    break;
+                }
+            }
+        }
+    }
+}
