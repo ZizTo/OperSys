59a002767783a8ab75c66d7d9257e2be35ed26d1
ZizTo <92594525+ZizTo@users.noreply.github.com>
2025-05-19 20:36:49 +0300

    wt

---
+++ lab5/Client/Client.cpp
@@ -0,0 +1,224 @@
+#include "structs.h"
+#include <vector>
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+HANDLE hNamedPipe;
+HANDLE NeedToRead;
+HANDLE ICanRead;
+HANDLE IDead;
+
+void CleanUp() {
+    CloseHandle(NeedToRead);
+    CloseHandle(ICanRead);
+    CloseHandle(IDead);
+    CloseHandle(hNamedPipe);
+}
+
+void DisplayEmployee(Employee emp) {
+    cout << "Employee#" << emp.num << " - "
+        << emp.name << " - "
+        << emp.hours << endl;
+}
+
+Employee ModifyEmployee(Employee data) {
+    cout << "Enter name: ";
+    string messageStr;
+    cin >> messageStr;
+    strncpy_s(data.name, messageStr.c_str(), sizeof(data.name) - 1);
+
+    cout << "Enter hours: ";
+    cin >> data.hours;
+
+    return data;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <client_id>" << endl;
+        return 1;
+    }
+    
+    int threadId = atoi(argv[1]);
+
+    hNamedPipe = CreateFile(
+        "\\\\.\\pipe\\pipe", // pipe name
+        GENERIC_READ | GENERIC_WRITE, // read and write access
+        FILE_SHARE_READ | FILE_SHARE_WRITE, // share read and write
+        (LPSECURITY_ATTRIBUTES)NULL, // default security
+        OPEN_EXISTING, // open existing pipe
+        FILE_ATTRIBUTE_NORMAL, // default attributes
+        (HANDLE)NULL // no template file
+    );
+    
+    if (hNamedPipe == INVALID_HANDLE_VALUE) {
+        cerr << "Failed to connect to pipe. Error: " << GetLastError() << endl;
+        return 1;
+    }
+    
+    Message message;
+    DWORD dwBytesWrite;
+    DWORD dwBytesRead;
+    
+    NeedToRead = OpenEvent(EVENT_ALL_ACCESS, FALSE, "ReadEvent");
+    if (!NeedToRead) {
+        cerr << "Failed to open ReadEvent. Error: " << GetLastError() << endl;
+        CloseHandle(hNamedPipe);
+        return 1;
+    }
+    
+    string readEventName = "Read" + to_string(threadId);
+    ICanRead = OpenEvent(EVENT_ALL_ACCESS, FALSE, readEventName.c_str());
+    if (!ICanRead) {
+        cerr << "Failed to open " << readEventName << ". Error: " << GetLastError() << endl;
+        CloseHandle(NeedToRead);
+        CloseHandle(hNamedPipe);
+        return 1;
+    }
+    
+    string deadEventName = "Dead" + to_string(threadId);
+    IDead = OpenEvent(EVENT_ALL_ACCESS, FALSE, deadEventName.c_str());
+    if (!IDead) {
+        cerr << "Failed to open " << deadEventName << ". Error: " << GetLastError() << endl;
+        CloseHandle(NeedToRead);
+        CloseHandle(ICanRead);
+        CloseHandle(hNamedPipe);
+        return 1;
+    }
+    
+    cout << "Client " << threadId << " connected successfully." << endl;
+    
+    while (true) {
+        cout << "q - exit, r - read, s - send: ";
+        char choose;
+        cin >> choose;
+        
+        switch (choose) {
+        case 'q':
+            SetEvent(IDead);
+            CleanUp();
+            cout << "Exiting client." << endl;
+            return 0;
+
+        case 'r':
+            message.id = threadId;
+            message.type = READ_REQUEST;
+            cout << "Enter employee ID you want to read: ";
+            cin >> message.employeeId;
+            
+            WriteFile(
+                hNamedPipe, // pipe handle
+                &message, // buffer for output
+                sizeof(Message), // bytes to write
+                &dwBytesWrite, // bytes written
+                (LPOVERLAPPED)NULL // synchronous write
+            );
+            
+            SetEvent(NeedToRead);
+            WaitForSingleObject(ICanRead, INFINITE);
+            ResetEvent(ICanRead);
+            
+            ReadFile(
+                hNamedPipe, // pipe handle
+                &message, // buffer for input
+                sizeof(Message), // bytes to read
+                &dwBytesRead, // bytes read
+                (LPOVERLAPPED)NULL // synchronous read
+            );
+            
+            if (message.type == BLOCK_RESPONSE) {
+                cout << "Access blocked - record is being modified." << endl;
+            }
+            else if (message.type == FAIL_READ) {
+                cout << "Employee ID not found." << endl;
+            }
+            else if (message.type == SUCCESS_READ) {
+                cout << "Record found:" << endl;
+                DisplayEmployee(message.employee);
+            }
+            break;
+            
+        case 's':
+            message.id = threadId;
+            message.type = WRITE_REQUEST;
+            cout << "Enter employee ID you want to modify: ";
+            cin >> message.employeeId;
+            
+            WriteFile(
+                hNamedPipe, // pipe handle
+                &message, // buffer for output
+                sizeof(Message), // bytes to write
+                &dwBytesWrite, // bytes written
+                (LPOVERLAPPED)NULL // synchronous write
+            );
+            
+            SetEvent(NeedToRead);
+            WaitForSingleObject(ICanRead, INFINITE);
+            ResetEvent(ICanRead);
+            
+            ReadFile(
+                hNamedPipe, // pipe handle
+                &message, // buffer for input
+                sizeof(Message), // bytes to read
+                &dwBytesRead, // bytes read
+                (LPOVERLAPPED)NULL // synchronous read
+            );
+            
+            if (message.type == BLOCK_RESPONSE) {
+                cout << "Access blocked - record is already being accessed." << endl;
+            }
+            else if (message.type == FAIL_READ) {
+                cout << "Employee ID not found." << endl;
+            }
+            else if (message.type == SUCCESS_READ) {
+                cout << "Current record:" << endl;
+                DisplayEmployee(message.employee);
+                
+                cout << "Enter new information:" << endl;
+                message.employee = ModifyEmployee(message.employee);
+                message.type = WRITE_REQUEST_READY;
+                message.id = threadId;
+                
+                WriteFile(
+                    hNamedPipe, // pipe handle
+                    &message, // buffer for output
+                    sizeof(Message), // bytes to write
+                    &dwBytesWrite, // bytes written
+                    (LPOVERLAPPED)NULL // synchronous write
+                );
+                
+                SetEvent(NeedToRead);
+                WaitForSingleObject(ICanRead, INFINITE);
+                ResetEvent(ICanRead);
+                
+                ReadFile(
+                    hNamedPipe, // pipe handle
+                    &message, // buffer for input
+                    sizeof(Message), // bytes to read
+                    &dwBytesRead, // bytes read
+                    (LPOVERLAPPED)NULL // synchronous read
+                );
+                
+                if (message.type == SUCCESS) {
+                    cout << "Record successfully updated:" << endl;
+                    DisplayEmployee(message.employee);
+                }
+                else {
+                    cout << "Failed to update record." << endl;
+                }
+            }
+            break;
+            
+        default:
+            cout << "Invalid option. Please try again." << endl;
+            break;
+        }
+    }
+    
+    // This should never be reached, but just in case
+    CleanUp();
+    return 0;
+}
--- lab5/Server/Server.cpp
+++ lab5/Server/Server.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,35 +17,44 @@ HANDLE hNamedPipe;
 HANDLE* YouCanReadNow;
 HANDLE* AllDead;
 
+// Track which records are being accessed and how (true for write, false for read)
+vector<pair<int, bool>> accessedRecords;
+
 void CleanUp() {
     for (auto pi : piv) {
-		CloseHandle(pi.hThread);
+        CloseHandle(pi.hThread);
         CloseHandle(pi.hProcess);
-	}
-	CloseHandle(NeedToRead);
+    }
+    CloseHandle(NeedToRead);
+    CloseHandle(hNamedPipe);
+    
+    for (int i = 0; i < piv.size(); i++) {
+        CloseHandle(YouCanReadNow[i]);
+        CloseHandle(AllDead[i]);
+    }
+    
+    delete[] YouCanReadNow;
+    delete[] AllDead;
 }
 
 void DisplayEmployeeFile() {
     ifstream file(binFl, ios::binary);
     Employee data;
-    cout << "\nGuys:\n";
-    while (!file.eof()) {
-        file.read(reinterpret_cast<char*>(&data), sizeof(data));
+    cout << "\nEmployees:\n";
+    while (file.read(reinterpret_cast<char*>(&data), sizeof(data))) {
         cout << "Employee#" << data.num << " - "
             << data.name << " - "
             << data.hours << endl;
     }
 
     cout << endl;
-
     file.close();
 }
 
 Employee FindEmployee(int id) {
     ifstream file(binFl, ios::binary);
     Employee data;
-    while (!file.eof()) {
-        file.read(reinterpret_cast<char*>(&data), sizeof(data));
+    while (file.read(reinterpret_cast<char*>(&data), sizeof(data))) {
         if (data.num == id) {
             file.close();
             return data;
@@ -57,20 +67,53 @@ Employee FindEmployee(int id) {
 }
 
 void ChangeEmployee(int id, Employee emp) {
+    fstream file(binFl, ios::binary | ios::in | ios::out);
+    Employee data;
+    while (!file.eof()) {
+        streampos pos = file.tellg();
+        file.read(reinterpret_cast<char*>(&data), sizeof(data));
+        if (file.eof()) break;
+        
+        if (data.num == id) {
+            file.seekp(pos);
+            file.write(reinterpret_cast<char*>(&emp), sizeof(emp));
+            break;
+        }
+    }
+    file.close();
+}
 
+bool isRecordAccessedForWrite(int employeeId) {
+    for (const auto& rec : accessedRecords) {
+        if (rec.first == employeeId && rec.second == true) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isRecordAccessed(int employeeId) {
+    return std::any_of(accessedRecords.begin(), accessedRecords.end(),
+        [employeeId](const pair<int, bool>& rec) { return rec.first == employeeId; });
+}
+
+void removeRecordAccess(int employeeId) {
+    accessedRecords.erase(
+        std::remove_if(accessedRecords.begin(), accessedRecords.end(),
+            [employeeId](const pair<int, bool>& rec) { return rec.first == employeeId; }),
+        accessedRecords.end());
 }
 
 int main() {
-	cout << "Enter file name: ";
-	cin >> binFl;
-	int count;
+    cout << "Enter file name: ";
+    cin >> binFl;
+    int count;
     cout << "Enter number of employees: ";
     cin >> count;
 
-	ofstream onF(binFl, ios::binary);
+    ofstream onF(binFl, ios::binary);
 
-    for (int i = 0; i < count; i++)
-	{
+    for (int i = 0; i < count; i++) {
         Employee data;
         data.num = i;
 
@@ -82,67 +125,60 @@ int main() {
         cout << "Enter hours: ";
         cin >> data.hours;
         
-		onF.write(reinterpret_cast<char*>(&data), sizeof(data));
-	}
+        onF.write(reinterpret_cast<char*>(&data), sizeof(data));
+    }
 
     onF.close();
     DisplayEmployeeFile();
 
-    cout << "Client kol: ";
+    cout << "Client count: ";
     int ClKol;
     cin >> ClKol;
     piv.resize(ClKol);
     siv.resize(ClKol);
 
-    for (auto si : siv) {
-        ZeroMemory(&si, sizeof(STARTUPINFO));
-        si.cb = sizeof(STARTUPINFO);
+    for (int i = 0; i < ClKol; i++) {
+        ZeroMemory(&siv[i], sizeof(STARTUPINFO));
+        siv[i].cb = sizeof(STARTUPINFO);
     }
 
     hNamedPipe = CreateNamedPipe(
-        "\\\\.\\pipe\\pipe", // имя канала
-        PIPE_ACCESS_DUPLEX, // читаем из канала
+        "\\\\.\\pipe\\pipe", // pipe name
+        PIPE_ACCESS_DUPLEX, // read/write access
         PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
-        PIPE_UNLIMITED_INSTANCES, // максимальное количество экземпляров канала
-        0, // размер выходного буфера по умолчанию
-        0, // размер входного буфера по умолчанию
-        INFINITE, // клиент ждет связь бесконечно долго
+        PIPE_UNLIMITED_INSTANCES, // max instances
+        0, // default output buffer size
+        0, // default input buffer size
+        INFINITE, // client waits indefinitely
         (LPSECURITY_ATTRIBUTES)NULL
     );
 
     string commLine = "Client98.exe ";
-    for (int i = 0; i < ClKol; i++)
-    {
-        cout << "2";
+    for (int i = 0; i < ClKol; i++) {
         CreateProcess(NULL, strdup((commLine + to_string(i)).c_str()), NULL, NULL, FALSE,
-			CREATE_NEW_CONSOLE, NULL, NULL, &siv[i], &piv[i]);
-        cout << "3";
-        if (ConnectNamedPipe(
-            hNamedPipe, // дескриптор канала
-            (LPOVERLAPPED)NULL // асинхронная связь
-        )) {
-            cout << "all cool";
+            CREATE_NEW_CONSOLE, NULL, NULL, &siv[i], &piv[i]);
+        
+        if (ConnectNamedPipe(hNamedPipe, (LPOVERLAPPED)NULL)) {
+            cout << "Connected to client " << i << endl;
         }
     }
-    
 
-    bool readOpen = true, modificOpen = true;
     NeedToRead = CreateEvent(NULL, false, false, "ReadEvent");
-    HANDLE* AllDead = new HANDLE[ClKol];
-    for (int i = 0; i < ClKol; i++)
-    {
-        AllDead[i] = CreateEvent(NULL, false, false, "Dead" + i);
+    AllDead = new HANDLE[ClKol];
+    for (int i = 0; i < ClKol; i++) {
+        AllDead[i] = CreateEvent(NULL, false, false, ("Dead" + to_string(i)).c_str());
     }
+    
     YouCanReadNow = new HANDLE[ClKol];
-    for (int i = 0; i < ClKol; i++)
-    {
-        YouCanReadNow[i] = CreateEvent(NULL, false, false, "Read" + i);
+    for (int i = 0; i < ClKol; i++) {
+        YouCanReadNow[i] = CreateEvent(NULL, false, false, ("Read" + to_string(i)).c_str());
     }
 
     while (true) {
         if (WaitForMultipleObjects(ClKol, AllDead, TRUE, 0) != WAIT_TIMEOUT) {
+            cout << "All clients have exited" << endl;
             break;
-        } 
+        }
 
         if (WaitForSingleObject(NeedToRead, 0) != WAIT_TIMEOUT) {
             ResetEvent(NeedToRead);
@@ -150,75 +186,81 @@ int main() {
             DWORD dwBytesWrite;
             Message message;
             ReadFile(
-                hNamedPipe, // дескриптор канала
-                &message, // адрес буфера для ввода данных
-                sizeof(Message), // число читаемых байтов
-                &dwBytesRead, // число прочитанных байтов
-                (LPOVERLAPPED)NULL // передача данных синхронная
+                hNamedPipe, // pipe handle
+                &message, // buffer for input
+                sizeof(Message), // bytes to read
+                &dwBytesRead, // bytes read
+                (LPOVERLAPPED)NULL // synchronous read
             );
 
             Message answ;
+            answ.id = message.id;
 
             switch (message.type) {
             case READ_REQUEST:
-                if (readOpen) {
+                if (!isRecordAccessedForWrite(message.employeeId)) {
+                    // Record is not being written to, can read
                     answ.employee = FindEmployee(message.employeeId);
                     if (answ.employee.num == -1) {
                         answ.type = FAIL_READ;
-                    }
-                    else {
+                    } else {
+                        accessedRecords.push_back(make_pair(message.employeeId, false)); // Mark as being read
                         answ.type = SUCCESS_READ;
                     }
-                }
-                else {
+                } else {
+                    // Record is being written to, can't read
                     answ.type = BLOCK_RESPONSE;
                 }
                 break;
+                
             case WRITE_REQUEST:
-                if (modificOpen) {
+                if (!isRecordAccessed(message.employeeId)) {
+                    // Record is not being accessed at all, can write
                     answ.employee = FindEmployee(message.employeeId);
                     if (answ.employee.num == -1) {
                         answ.type = FAIL_READ;
-                    }
-                    else {
-                        readOpen = false;
-                        modificOpen = false;
+                    } else {
+                        accessedRecords.push_back(make_pair(message.employeeId, true)); // Mark as being written
                         answ.type = SUCCESS_READ;
                     }
-                }
-                else {
+                } else {
+                    // Record is being accessed, can't write
                     answ.type = BLOCK_RESPONSE;
                 }
                 break;
+                
             case WRITE_REQUEST_READY:
                 ChangeEmployee(message.employee.num, message.employee);
                 answ.type = SUCCESS;
-                message.employee = FindEmployee(message.employeeId);
-                readOpen = true;
-                modificOpen = true;
+                answ.employee = message.employee; // Send the updated employee back
+                
+                // Remove the record from access control
+                removeRecordAccess(message.employeeId);
                 break;
 
             default:
-                cerr << "err";
-                return 1;
+                cerr << "Unknown message type: " << message.type << endl;
+                answ.type = FAIL_READ;
                 break;
             }
 
             WriteFile(
-                hNamedPipe, // дескриптор канала
-                &answ, // адрес буфера для вывода данных
-                sizeof(Message), // число записываемых байтов
-                &dwBytesWrite, // число записанных байтов
-                (LPOVERLAPPED)NULL // передача данных синхронная
+                hNamedPipe, // pipe handle
+                &answ, // buffer for output
+                sizeof(Message), // bytes to write
+                &dwBytesWrite, // bytes written
+                (LPOVERLAPPED)NULL // synchronous write
             );
 
             SetEvent(YouCanReadNow[message.id]);
         }
     }
     
-    char a;
     DisplayEmployeeFile();
-    cout << "Enter to close";
-    cin >> a;
+    cout << "Press Enter to close...";
+    cin.ignore();
+    cin.get();
     CleanUp();
+    
+    return 0;
 }
